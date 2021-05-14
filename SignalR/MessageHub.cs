8f59e008e44ab1bdfdb696f2076765369c7a78c2
vivek <vivek@athenaeum.in>
2021-05-14 22:49:23 +0530

    done

---
+++ SignalR/MessageHub.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCore.SignalR.Demo.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.SignalR.Demo.SignalR
+{
+    public class MessageHub : Hub
+    {
+        public async Task SendMessageAsync(string messageText)
+        {
+            var message = new MessageViewModel
+            {
+                Id = Guid.NewGuid(),
+                Message = messageText,
+                TimeStamp = DateTimeOffset.UtcNow
+            };
+
+            //  send all
+            await Clients.All.SendAsync("ReceiveMessage", message.Id, message.Message, message.TimeStamp);
+        }
+    }
+}
---
+++ ViewModels/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore.SignalR.Demo.ViewModels
+{
+    public class MessageViewModel
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset TimeStamp { get; set; }
+    }
+}
