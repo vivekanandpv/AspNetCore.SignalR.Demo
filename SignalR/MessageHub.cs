4232ae5df50f517dd90872235c85eeac91e362ea
vivek <vivek@athenaeum.in>
2021-05-15 07:39:02 +0530

    done

--- SignalR/MessageHub.cs
+++ SignalR/MessageHub.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetCore.SignalR.Demo.ViewModels;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,25 @@ namespace AspNetCore.SignalR.Demo.SignalR
 {
     public class MessageHub : Hub
     {
+        private ILogger<MessageHub> _logger;
+
+        public MessageHub(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<MessageHub>();
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", Guid.NewGuid(), "Welcome to SignalR");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            _logger.LogInformation("Client disconnected");
+            await base.OnDisconnectedAsync(exception);
+        }
+
         public async Task SendMessageAsync(string messageText)
         {
             var message = new MessageViewModel
